da833d199596c2a1143ee3b2a9665be00dde3be1
23-Pravallika <learngain23@gmail.com>
2024-02-08 15:57:29 +0530

    AutoPush

--- sample.tf
+++ sample.tf
@@ -1,4 +1,22 @@
-output "terraform_URL" {
+output "terraform_URL"{
     value="https://developer.hashicorp.com/terraform/language/values/outputs"
+}
+
+# variables
+
+
+varibale "Sample_vars"{
+    default="This is demo on vars"
+}
+
+variable "string_type" {
+ description = "This is a variable of type string"
+ type        = string
+ default     = "Default string value for this variable"
+}
+
+output "vars_output"{
+    value=var.Sample_vars
+    value=var.string_type
 }
 
