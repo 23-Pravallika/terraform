3d1ce360f3522c0c1725724477f68dcaefe4ab20
23-Pravallika <learngain23@gmail.com>
2024-02-09 13:23:37 +0530

    AutoPush

--- sample.tf
+++ sample.tf
@@ -168,9 +168,9 @@ output "number_op" {
 }
 
 # This variable value will be supplied from the command line
-variable "provider" {}
+variable "provider_name" {}
 
-output "provider_name" {
-    value = "Name of the provider is ${var.provider}"
+output "provider_name_op" {
+    value = "Name of the provider is ${var.provider_name}"
 }
 
