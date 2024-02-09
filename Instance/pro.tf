0b387a3e578bd2db18eeb1d92c4a50330bf88ddc
23-Pravallika <learngain23@gmail.com>
2024-02-09 16:21:20 +0530

    AutoPush

--- Instance/pro.tf
+++ Instance/pro.tf
@@ -2,12 +2,3 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-029c63fec721135c4"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "terraform"
-  }
-}
-
