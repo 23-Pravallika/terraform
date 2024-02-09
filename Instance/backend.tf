85510be5acee0d9503894055f0faa5c2fe4cefea
23-Pravallika <learngain23@gmail.com>
2024-02-09 18:23:47 +0530

    AutoPush

--- Instance/backend.tf
+++ Instance/backend.tf
@@ -1,11 +1,11 @@
-# Declaring the remote backend; This will keep the state files in a remote s3 buckets and this will let team collaboration
-# terraform {
-#   backend "s3" {
-#     bucket = "tfstate-bucket"
-#     key    = "dev/ec2"
-#     region = "us-east-1"
-#   }
-# }
+#Declaring the remote backend; This will keep the state files in a remote s3 buckets and this will let team collaboration
+terraform {
+  backend "s3" {
+    bucket = "buck-tf-state"
+    key    = "dev/ec2"
+    region = "us-east-1"
+  }
+}
 
 
 # terraform {
