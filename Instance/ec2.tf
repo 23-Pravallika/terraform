6a25dfe9d7b45c92ad5866d7fbfc282d7d95e632
23-Pravallika <learngain23@gmail.com>
2024-02-09 18:31:25 +0530

    AutoPush

--- Instance/ec2.tf
+++ Instance/ec2.tf
@@ -1,15 +1,15 @@
-resource "aws_instance" "web" {
-  ami           = "ami-029c63fec721135c4"
-  instance_type = "t2.micro"
-  #vpc_security_group_ids = [aws_security_group.allow-all-ssh.id]
+# resource "aws_instance" "web" {
+#   ami           = "ami-029c63fec721135c4"
+#   instance_type = "t2.micro"
+#   #vpc_security_group_ids = [aws_security_group.allow-all-ssh.id]
 
-  tags = {
-    Name = "terraform-server"
-  }
-}
+#   tags = {
+#     Name = "terraform-server"
+#   }
+# }
 
-# Block to print the attribute 
-output "ip"{
-    value=aws_instance.web.private_ip
-}
+# # Block to print the attribute 
+# output "ip"{
+#     value=aws_instance.web.private_ip
+# }
 
