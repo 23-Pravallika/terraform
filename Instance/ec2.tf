416a9408b1e10cf962aa6eedf0f1ae2cb7850873
23-Pravallika <learngain23@gmail.com>
2024-02-09 16:35:03 +0530

    AutoPush

--- Instance/ec2.tf
+++ Instance/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-029c63fec721135c4"
   instance_type = "t2.micro"
-  
+  security_groups = "sg-0705c17ba7dd211f5"
 
   tags = {
     Name = "terraform-server"
