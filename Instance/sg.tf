3f1eafd7a34b8fb8a7e316659d208f9fb9c66553
23-Pravallika <learngain23@gmail.com>
2024-02-09 18:12:17 +0530

    AutoPush

--- Instance/sg.tf
+++ Instance/sg.tf
@@ -1,26 +1,26 @@
-resource "aws_security_group" "allow-all-ssh" {
- ## Creates Security Group 
- name        = "allow-all-ssh"
- description = "allow ssh"
+# resource "aws_security_group" "allow-all-ssh" {
+#  ## Creates Security Group 
+#  name        = "allow-all-ssh"
+#  description = "allow ssh"
  
-ingress {
-   description = "SSH"
-   from_port   = 22
-   to_port     = 22
-   protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+# ingress {
+#    description = "SSH"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-egress {
-   from_port   = 0
-   to_port     = 0
-   protocol    = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+# egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-tags = {
-    Name = "allow-all-ssh"
-  }
-}
+# tags = {
+#     Name = "allow-all-ssh"
+#   }
+#}
 
 
