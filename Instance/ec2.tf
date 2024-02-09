33a96dc26014664a136103d014e1b822d3f06fb6
23-Pravallika <learngain23@gmail.com>
2024-02-09 16:31:19 +0530

    AutoPush

--- Instance/ec2.tf
+++ Instance/ec2.tf
@@ -1,8 +1,14 @@
 resource "aws_instance" "web" {
   ami           = "ami-029c63fec721135c4"
   instance_type = "t2.micro"
+  
 
   tags = {
     Name = "terraform-server"
   }
+}
+
+# Block to print the attribute 
+output "ip"{
+    value=aws_instance.web.private_ip
 }
