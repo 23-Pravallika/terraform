f8980fed9f5c8c79ec0b1c4f123e54e6a5f6d2a8
23-Pravallika <learngain23@gmail.com>
2024-02-10 11:38:14 +0530

    AutoPush

--- modules-in-tf/main.tf
+++ modules-in-tf/main.tf
@@ -10,11 +10,11 @@ module "sg_module" {
 
 # Printing the output from ec2 module
 output "ip"{
-    value=aws_instance.web.private_ip
+    value=module.ec2_module.ip
 }
 
 # Printing the output from sg module
 output "sg_desc"{
-    value=aws_security_group.allow-all-ssh.description
+    value=module.sg_module.sg_desc
 }
 
