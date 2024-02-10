26df8ca13f81e4e28d0f02d2c6cd4cc166a06bdf
23-Pravallika <learngain23@gmail.com>
2024-02-10 11:18:07 +0530

    AutoPush

--- modules-in-tf/ec2/main.tf
+++ modules-in-tf/ec2/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "web" {
   ami           = "ami-029c63fec721135c4"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids = [var.sg]
 
   tags = {
     Name = "terraform-module"
   }
 }
 
-variable "sg_id" {}
+variable "sg" {}
 
 output "ip"{
     value=aws_instance.web.private_ip
--- modules-in-tf/main.tf
+++ modules-in-tf/main.tf
@@ -1,7 +1,6 @@
 module "ec2_module" {
     source = "./ec2"
-    sg = module.sg.sg_id
-    
+    sg = module.sg_module.sg_id
 }
 
 module "sg_module" {
