0cafe75c6496f26b9fc1819a88aadb2f3d817e13
23-Pravallika <learngain23@gmail.com>
2024-02-10 11:33:51 +0530

    AutoPush

--- modules-in-tf/sg/backend.tf
+++ modules-in-tf/sg/backend.tf
@@ -6,6 +6,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
+# Declaring the remote backend; This will keep the state files in a remote s3 buckets and this will let team collaboration
 terraform {
   backend "s3" {
     bucket = "buck-tf-state"
