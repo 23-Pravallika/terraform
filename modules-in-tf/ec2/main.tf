98a39bdfe28b71eca4407b1c02285e2aa3c3dd5f
23-Pravallika <learngain23@gmail.com>
2024-02-11 14:58:42 +0530

    AutoPush

--- modules-in-tf/ec2/main.tf
+++ modules-in-tf/ec2/main.tf
@@ -6,7 +6,25 @@ resource "aws_instance" "web" {
   tags = {
     Name = "terraform-module"
   }
+  
+# Declaring the remote provisioner inside the resource
+ provisioner "remote-exec" {
+       connection {
+          type     = "ssh"
+          user     = "centos"
+          password = "DevOps321"
+          host     = self.private_ip
+  }
+    inline = [
+      "ansible-pull -U https://github.com/23-Pravallika/Ansible.git robo-pull.yml -e ENV=dev -e COMPONENT=mongodb"
+    ]
+  }
+# Declaring the remote provisioner inside the resource
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip}"
+  }
 }
+
 
 variable "sg" {}
 
